d3275be79f9c832aa9b0e23c163333228ec251cb
Haashir <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-02-22 07:48:56 +0500

    Update tests.py

--- Flight/tests.py
+++ Flight/tests.py
@@ -50,11 +50,4 @@ class FLightTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context['flights'].count(),3)
 
-    def test_valid_flight_page(self):
-        a1 = Airport.objects.get(code="ABC")
-        a2 = Airport.objects.get(code="DEF")
-        f=Flight.objects.get(origin=a1,destination=a1)
 
-        c=Client()
-        response=c.get(f"/flights/{f.id}")
-        self.assertEquals(response.status_code,200)
