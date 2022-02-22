2ada82336f8076586bc8bf73fea8b446e49f464b
Haashir <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-02-22 07:41:22 +0500

    Update tests.py

--- Flight/tests.py
+++ Flight/tests.py
@@ -26,13 +26,14 @@ class FLightTestCase(TestCase):
         a = Airport.objects.get(code="DEF")
         self.assertEqual(a.arrivals.count(),2)
 
-    def test_valid_flight(self):
+    def test_valid_flight_page(self):
         a1 = Airport.objects.get(code="ABC")
-        a2= Airport.objects.get(code="DEF")
-        f=Flight.objects.create(origin=a1,destination=a2,duration=100)
-        self.assertTrue(f.is_valid_flight())
-    
-    def test_invalid_flight_destination(self):
+        a2 = Airport.objects.get(code="DEF")
+        f=Flight.objects.first(origin=a1,destination=a2)
+
+        c=Client()
+        response=c.get(f"/flights/{f.id}")
+        self.assertEquals(response.status_code,200)
         a1 = Airport.objects.get(code="ABC")
         f=Flight.objects.create(origin=a1,destination=a1,duration=100)
         self.assertFalse(f.is_valid_flight())
